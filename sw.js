a074071a9ecc12b51dfb4cbd5553df3e9193169f
messi-wxx <601952658@qq.com>
2020-06-18 11:50:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+importScripts("js/workbox-sw.js");
 var cacheStorageKey = 'test-pwa-1'
 var cacheList = [
 	'/',
