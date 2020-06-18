71478254a3ed0cba9f17898183832b076d58131c
messi-wxx <601952658@qq.com>
2020-06-18 14:42:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,56 @@
-importScripts("js/workbox-sw.js");
-var cacheStorageKey = 'test-pwa-1'
-var cacheList = [
-	'/',
-	'index.html',
-	'css/main.css',
-	'img/ic_s.png'
-]
-self.addEventListener('install', e => {
-	e.waitUntil(
-		caches.open(cacheStorageKey)
-		.then(cache => cache.addAll(cacheList))
-		.then(() => self.skipWaiting())
-	)
-})
-self.addEventListener('fetch', function(e) {
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-			if (response != null) {
-				return response
-			}
-			return fetch(e.request.url)
+// 缓存静态文件
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		// 缓存指定文件
+		caches.open('v1').then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/css/main.css',
+				'/app.js',
+				'/img/ic_s.png',
+			]);
 		})
-	)
-})
-self.addEventListener('activate', function(e) {
-	e.waitUntil(
-		//获取所有cache名称
-		caches.keys().then(cacheNames => {
+	);
+});
+
+// 缓存接口数据
+self.addEventListener('fetch', function(event) {
+	event.respondWith(caches.match(event.request).then(function(response) {
+		// 匹配到请求
+		if (response !== undefined) {
+			return response;
+		} else {
+			return fetch(event.request).then(function(response) {
+				// 缓存响应数据
+				let responseClone = response.clone();
+
+				caches.open('v1').then(function(cache) {
+					cache.put(event.request, responseClone);
+				});
+				return response;
+			}).catch(function() {
+				return caches.match('/img/timg.jfif');
+			});
+		}
+	}));
+});
+
+// 更新缓存
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
 			return Promise.all(
-				// 获取所有不同于当前版本名称cache下的内容
-				cacheNames.filter(cacheNames => {
-					return cacheNames !== cacheStorageKey
-				}).map(cacheNames => {
-					return caches.delete(cacheNames)
+				cacheNames.map(function(cacheName) {
+					// 如果有更新
+					if (cacheName !== 'v1') {
+						return caches.delete(cacheName);
+					}
 				})
-			)
-		}).then(() => {
+			);
+		})
+		.then(function() {
 			return self.clients.claim()
 		})
-	)
-})
+	);
+});
